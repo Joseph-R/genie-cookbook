bd436aa3ee7c25c9f64b09df19ec5a7cb9bbc8ec
Joseph-R <jreid@korrelate.com>
2016-01-20 17:02:01 -0500

    Pull in Hadoop cookbook to install clients

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures genie-cookbook'
 long_description 'Installs/Configures genie-cookbook'
 version '0.1.0'
 
-depends 'database', '~> 4.0.6'          # Install/configure local postgres DB.
-
+depends 'database', '~> 4.0.6'          # Install/configure local postgres DB
+depends 'hadoop', '~> 2.2.0'            # Install Hadoop clients and binaries
 depends 'java', '= 1.26.0'              # Install Oracle JDK for Java version
 depends 'tomcat', '~> 1.0.1'            # Install Tomcat
