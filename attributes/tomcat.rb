2cd7f0cf27e182b553a429e5179e92bd32206f45
Joseph-R <jreid@korrelate.com>
2016-01-20 17:06:46 -0500

    Switch Genie port to 7000

--- attributes/tomcat.rb
+++ attributes/tomcat.rb
@@ -2,5 +2,5 @@
 # See https://github.com/chef-cookbooks/tomcat
 
 node.default['tomcat']['base_version'] = '7'
-node.default['genie']['tomcat']['port'] = "8080"
+node.default['genie']['tomcat']['port'] = "7000"
 node.default['genie']['tomcat']['ssl_port'] = "8443"
