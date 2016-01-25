8a65e2616d59ed0a4172fb2f9003fb5c92297705
Joseph-R <jreid@korrelate.com>
2016-01-25 16:21:11 -0500

    Clarify comments in Hadoop attribute file

--- attributes/hadoop.rb
+++ attributes/hadoop.rb
@@ -1,6 +1,7 @@
 # Set hadoop attributes
-# REQUIRED: Hadoop distribution type and version.
-# See https://github.com/caskdata/hadoop_cookbook for details.
+# Default assumes HDP distribution.  Can be overwritten in a wrapper cookbook to
+# any option available in https://github.com/caskdata/hadoop_cookbook.
 
 node.default['hadoop']['distribution'] = "hdp"
 node.default['hadoop']['distribution_version'] = "2.2.3"
+node.default['hadoop']['home'] = '/usr/lib/hadoop'
