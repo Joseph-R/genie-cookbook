a316614509f0c14b0c47c76d713a631d6aa988cf
Joseph-R <jreid@korrelate.com>
2016-01-25 16:20:48 -0500

    Add wrapper attributes to test VM

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,12 +2,9 @@
 node.default['genie']['version'] = "2.2.3"
 
 # Performance tuning.  
-node.default['genie']['max_running_jobs'] = "50"
-node.default['genie']['forward_jobs_threshold'] = "60"
+node.default['genie']['max_running_jobs'] = "30"
+node.default['genie']['forward_jobs_threshold'] = "20"
 
 # AWS
 node.default['genie']['access_key_id'] = nil
 node.default['genie']['secret_access_key'] = nil
-
-# Hadoop parameters
-node.default['hadoop']['home'] = "/usr/lib/hadoop"
