386c1299fe2d54b50c2d8a58fbc1a32716804458
Joseph-R <jreid@korrelate.com>
2016-01-26 17:21:47 -0500

    WiP - minor housekeeping

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@
 node.default['genie']['version'] = "2.2.3"
 
 # Performance tuning.  
-node.default['genie']['max_running_jobs'] = "30"
-node.default['genie']['forward_jobs_threshold'] = "20"
+node.default['genie']['max_running_jobs'] = 30
+node.default['genie']['forward_jobs_threshold'] = 20
 
 # AWS
 node.default['genie']['access_key_id'] = nil
--- attributes/tomcat.rb
+++ attributes/tomcat.rb
@@ -4,11 +4,10 @@
 node.default['tomcat']['base_version'] = '7'
 node.default['tomcat']['port'] = 7000
 node.default['tomcat']['ssl_port'] = 8443
+node.default['tomcat']['java_options'] = "${JAVA_OPTS} -Xmx128M -Djava.awt.headless=true"
 
 if node['cloud'] == true
   node.default['tomcat']['catalina_options'] = "-Dspring.profiles.active=prod -Darchaius.deployment.applicationId=genie -Dnetflix.datacenter=cloud -Darchaius.deployment.environment=prod"
 else
   node.default['tomcat']['catalina_options'] = "-Dspring.profiles.active=prod -Darchaius.deployment.applicationId=genie -Darchaius.deployment.environment=prod"
 end
-
-# node.default['tomcat']['java_options'] = 
