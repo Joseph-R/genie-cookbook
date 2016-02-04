dfe9376f25892dc4c8feda4dc5cc59781f7ea91a
Joseph-R <jreid@korrelate.com>
2016-02-04 09:51:32 -0500

    Clean up Postgres attributes

--- attributes/postgres.rb
+++ attributes/postgres.rb
@@ -12,13 +12,14 @@ node.default['postgresql']['password']['postgres'] = "starfish"
 node.default['postgresql']['server']['config_change_notify'] = :reload
 node.default['postgresql']['config']['listen_addresses'] = 'localhost'
 
-# Legacy code.  Deprecate?
-# node.default['genie']['postgres']['java_class'] = "org.postgresql.Driver"
-# node.default['genie']['postgres']['connection_properties'] = "minPoolSize=5,acquireRetryAttempts=3,maxPoolSize=20,testConnectionOnCheckout=true"
-# node.default['genie']['postgres']['connection_string'] = "postgresql://#{node['genie']['postgres']['server']}:#{node['genie']['postgres']['port']}/#{node['genie']['postgres']['database']}?autoReconnect=true&amp;ssl=true&amp;sslfactory=org.postgresql.ssl.NonValidatingFactory"
-
 # Genie-specific configs.  See genie-jpa.xml template in default.rb.
 #node.default['genie']['postgres']['data_source_class'] = "org.springframework.jdbc.datasource.DriverManagerDataSource"
 node.default['genie']['postgres']['data_source_class'] = "org.apache.commons.dbcp2.BasicDataSource"
 node.default['genie']['postgres']['driver_class'] = "org.postgresql.Driver"
 node.default['genie']['postgres']['URL_connection_string'] = "jdbc:postgresql://localhost:#{node['genie']['postgres']['port']}/#{node['genie']['postgres']['database']}"
+
+
+# Legacy code.  Deprecate?
+# node.default['genie']['postgres']['java_class'] = "org.postgresql.Driver"
+# node.default['genie']['postgres']['connection_properties'] = "minPoolSize=5,acquireRetryAttempts=3,maxPoolSize=20,testConnectionOnCheckout=true"
+# node.default['genie']['postgres']['connection_string'] = "postgresql://#{node['genie']['postgres']['server']}:#{node['genie']['postgres']['port']}/#{node['genie']['postgres']['database']}?autoReconnect=true&amp;ssl=true&amp;sslfactory=org.postgresql.ssl.NonValidatingFactory"
