7b469cdf4f3122fd6b1531350c254c24518a4fb7
Joseph-R <jreid@korrelate.com>
2016-02-04 09:49:42 -0500

    Bump Java cb version

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,5 @@ version '0.1.0'
 
 depends 'database', '~> 4.0.6'          # Install/configure local postgres DB
 depends 'hadoop', '~> 2.2.0'            # Install Hadoop clients and binaries
-depends 'java', '= 1.26.0'              # Install Oracle JDK for Java version
+depends 'java', '~> 1.39.0'             # Install Oracle JDK for Java version
 depends 'tomcat', '~> 1.0.1'            # Install Tomcat
